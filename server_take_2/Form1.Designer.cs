847d19730a5a8a179277908faaef4252ecd4e1c3
sagivd <sagivdev@gmail.com>
2017-09-14 23:04:28 +0300

    first step-ish
    Signed-off-by: sagivd <sagivdev@gmail.com>

---
+++ server_take_2/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace server_take_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxRX = new System.Windows.Forms.TextBox();
+            this.textBoxTX = new System.Windows.Forms.TextBox();
+            this.listViewConn = new System.Windows.Forms.ListView();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonClearList = new System.Windows.Forms.Button();
+            this.radioButtonUDP = new System.Windows.Forms.RadioButton();
+            this.radioButtonTCP = new System.Windows.Forms.RadioButton();
+            this.radioButtonSSL = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.buttonListen = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.checkBoxEcho = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxRX
+            // 
+            this.textBoxRX.Location = new System.Drawing.Point(51, 114);
+            this.textBoxRX.Multiline = true;
+            this.textBoxRX.Name = "textBoxRX";
+            this.textBoxRX.Size = new System.Drawing.Size(249, 89);
+            this.textBoxRX.TabIndex = 0;
+            this.textBoxRX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxRX_KeyPress);
+            // 
+            // textBoxTX
+            // 
+            this.textBoxTX.Location = new System.Drawing.Point(51, 232);
+            this.textBoxTX.Multiline = true;
+            this.textBoxTX.Name = "textBoxTX";
+            this.textBoxTX.Size = new System.Drawing.Size(249, 89);
+            this.textBoxTX.TabIndex = 1;
+            // 
+            // listViewConn
+            // 
+            this.listViewConn.CheckBoxes = true;
+            this.listViewConn.Location = new System.Drawing.Point(51, 380);
+            this.listViewConn.Name = "listViewConn";
+            this.listViewConn.Size = new System.Drawing.Size(249, 99);
+            this.listViewConn.TabIndex = 2;
+            this.listViewConn.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(319, 114);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(319, 232);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonClearList
+            // 
+            this.buttonClearList.Location = new System.Drawing.Point(319, 380);
+            this.buttonClearList.Name = "buttonClearList";
+            this.buttonClearList.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearList.TabIndex = 5;
+            this.buttonClearList.Text = "Clear";
+            this.buttonClearList.UseVisualStyleBackColor = true;
+            this.buttonClearList.Click += new System.EventHandler(this.buttonClearList_Click);
+            // 
+            // radioButtonUDP
+            // 
+            this.radioButtonUDP.AutoSize = true;
+            this.radioButtonUDP.Checked = true;
+            this.radioButtonUDP.Location = new System.Drawing.Point(51, 34);
+            this.radioButtonUDP.Name = "radioButtonUDP";
+            this.radioButtonUDP.Size = new System.Drawing.Size(48, 17);
+            this.radioButtonUDP.TabIndex = 6;
+            this.radioButtonUDP.TabStop = true;
+            this.radioButtonUDP.Text = "UDP";
+            this.radioButtonUDP.UseVisualStyleBackColor = true;
+            this.radioButtonUDP.CheckedChanged += new System.EventHandler(this.radioButtonUDP_CheckedChanged);
+            // 
+            // radioButtonTCP
+            // 
+            this.radioButtonTCP.AutoSize = true;
+            this.radioButtonTCP.Location = new System.Drawing.Point(51, 57);
+            this.radioButtonTCP.Name = "radioButtonTCP";
+            this.radioButtonTCP.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonTCP.TabIndex = 7;
+            this.radioButtonTCP.Text = "TCP";
+            this.radioButtonTCP.UseVisualStyleBackColor = true;
+            this.radioButtonTCP.CheckedChanged += new System.EventHandler(this.radioButtonTCP_CheckedChanged);
+            // 
+            // radioButtonSSL
+            // 
+            this.radioButtonSSL.AutoSize = true;
+            this.radioButtonSSL.Location = new System.Drawing.Point(51, 80);
+            this.radioButtonSSL.Name = "radioButtonSSL";
+            this.radioButtonSSL.Size = new System.Drawing.Size(45, 17);
+            this.radioButtonSSL.TabIndex = 8;
+            this.radioButtonSSL.Text = "SSL";
+            this.radioButtonSSL.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "IP Address: 192.168.1.1";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(177, 42);
+            this.textBoxPort.MaxLength = 6;
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPort.TabIndex = 10;
+            this.textBoxPort.Text = "9001";
+            this.textBoxPort.TextChanged += new System.EventHandler(this.textBoxPort_TextChanged);
+            this.textBoxPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPort_KeyPress);
+            // 
+            // buttonListen
+            // 
+            this.buttonListen.Location = new System.Drawing.Point(319, 40);
+            this.buttonListen.Name = "buttonListen";
+            this.buttonListen.Size = new System.Drawing.Size(75, 23);
+            this.buttonListen.TabIndex = 11;
+            this.buttonListen.Text = "Start Listen";
+            this.buttonListen.UseVisualStyleBackColor = true;
+            this.buttonListen.Click += new System.EventHandler(this.buttonListen_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(319, 69);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 12;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // checkBoxEcho
+            // 
+            this.checkBoxEcho.AutoSize = true;
+            this.checkBoxEcho.Location = new System.Drawing.Point(177, 74);
+            this.checkBoxEcho.Name = "checkBoxEcho";
+            this.checkBoxEcho.Size = new System.Drawing.Size(87, 17);
+            this.checkBoxEcho.TabIndex = 13;
+            this.checkBoxEcho.Text = "Echo Enable";
+            this.checkBoxEcho.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 509);
+            this.Controls.Add(this.checkBoxEcho);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonListen);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButtonSSL);
+            this.Controls.Add(this.radioButtonTCP);
+            this.Controls.Add(this.radioButtonUDP);
+            this.Controls.Add(this.buttonClearList);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listViewConn);
+            this.Controls.Add(this.textBoxTX);
+            this.Controls.Add(this.textBoxRX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxRX;
+        private System.Windows.Forms.TextBox textBoxTX;
+        private System.Windows.Forms.ListView listViewConn;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonClearList;
+        private System.Windows.Forms.RadioButton radioButtonUDP;
+        private System.Windows.Forms.RadioButton radioButtonTCP;
+        private System.Windows.Forms.RadioButton radioButtonSSL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Button buttonListen;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.CheckBox checkBoxEcho;
+    }
+}
+
---
+++ server_take_2/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace server_take_2
+{
+    public enum Protocol_t
+    {
+        PROTO_TCP,
+        PROTO_UDP,
+        PROTO_SSL,
+    };
+
+    public partial class Form1 : Form
+    {
+        delegate void update_RX_text_delegate(string str);
+        delegate void add_client_to_list_delegate(ListViewItem itm);
+
+        private static Form1 form = null;
+        public Protocol_t selectedProto;
+        ConnectionManager CM;
+
+        public Form1()
+        {
+            InitializeComponent();
+            selectedProto = Protocol_t.PROTO_UDP;
+            CM = new ConnectionManager(this);
+
+            listViewConn.View = View.Details;
+            listViewConn.Columns.Add("IP Address", 150);
+            listViewConn.Columns.Add("Port", 100);
+
+            form = this;
+        }
+
+        private void radioButtonTCP_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonTCP.Checked)
+                selectedProto = Protocol_t.PROTO_TCP;
+        }
+
+        private void radioButtonUDP_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonUDP.Checked)
+                selectedProto = Protocol_t.PROTO_UDP;
+        }
+
+        private void textBoxPort_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBoxPort.Text, "  ^ [0-9]"))
+            {
+                textBoxPort.Text = "";
+            }
+        }
+
+        private void textBoxPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxRX.Text = "";
+        }
+
+        private void textBoxRX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (!CM.is_listening)
+            {
+                return;
+            }
+
+            CM.sendTextToClients(textBoxTX.Text);
+    
+        }
+
+        private void buttonListen_Click(object sender, EventArgs e)
+        {
+            CM.startListening();
+            buttonListen.Enabled = false;
+            buttonStop.Enabled = true;
+            radioButtonTCP.Enabled = false;
+            radioButtonUDP.Enabled = false;
+            radioButtonSSL.Enabled = false;
+            textBoxPort.Enabled = false;
+        }
+
+        public bool got_port(out UInt16 port)
+        {
+            if (UInt16.TryParse(textBoxPort.Text, out port))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void stat_updateRXTextBox(string str)
+        {
+            if (form != null)
+                form.textBoxRX.Text += str;
+        }
+
+        public void updateRXTextBox(string str)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new update_RX_text_delegate(stat_updateRXTextBox), new object[] { str });
+                return;
+            }
+
+            textBoxRX.Text += str;
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            CM.stopListening();
+            buttonStop.Enabled = false;
+            buttonListen.Enabled = true;
+        }
+
+        static private void stat_addNewClient(ListViewItem itm)
+        {
+            if (form == null)
+                return;
+
+            itm.Checked = true;
+            form.listViewConn.Items.Add(itm);
+        }
+
+        public void addNewClient(ListViewItem itm)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new add_client_to_list_delegate(stat_addNewClient), new object[] { itm });
+                return;
+            }
+
+            itm.Checked = true;
+            listViewConn.Items.Add(itm);
+        }
+
+        public bool clientIsInList(ListViewItem itm)
+        {
+            return listViewConn.Items.Contains(itm);
+        }
+
+        public ListView.CheckedListViewItemCollection getActiveClients()
+        {
+            return listViewConn.CheckedItems;
+        }
+
+        private void buttonClearList_Click(object sender, EventArgs e)
+        {
+            listViewConn.Items.Clear();
+        }
+        
+    }
+
+    public class UdpState
+    {
+        public UdpClient u;
+        public IPEndPoint e;
+        public Form1 p;
+        public AutoResetEvent a;
+        public ConnectionManager c;
+    }
+
+    public class ConnectionManager
+    {
+        Form1 parent;
+        public bool is_listening;
+        bool stop;
+        UdpClient listener;
+        Thread listenThread;
+        IPEndPoint listenEndPoint;
+        static AutoResetEvent autoEvent = new AutoResetEvent(false);
+        List<IPEndPoint> clientList;
+        UdpState stateHandler;
+
+        public ConnectionManager(Form1 parent)
+        {
+            this.parent = parent;
+
+            is_listening = false;
+            stop = false;
+
+            clientList = new List<IPEndPoint>();
+        }
+        
+        public void startListening()
+        {
+            if (is_listening)
+            {
+                // should not be here
+                MessageBox.Show("already listening");
+                return;
+            }
+
+            listenThread = new Thread(new ThreadStart(listenThreadMethodUDP));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+            is_listening = true;
+        }
+
+        private void listenThreadMethodUDP()
+        {
+            UInt16 listenPort;
+
+            if (parent.selectedProto == Protocol_t.PROTO_UDP)
+            {
+                if (!parent.got_port(out listenPort))
+                {
+                    MessageBox.Show("invalid port");
+                    return;
+                }
+            }
+            else
+            {
+                //not supported
+                MessageBox.Show("not supported");
+                return;
+            }
+
+            listener = new UdpClient(listenPort);
+            listenEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+
+            stateHandler = new UdpState();
+            stateHandler.p = parent;
+            stateHandler.u = listener;
+            stateHandler.e = listenEndPoint;
+            stateHandler.a = autoEvent;
+            stateHandler.c = this;
+
+            
+            do
+            {
+                listener.BeginReceive(new AsyncCallback(ReceiveCallback), stateHandler);
+                autoEvent.WaitOne(Timeout.Infinite);
+            } while(!stop);
+
+            listener.Close();
+            is_listening = false;
+        }
+
+        public bool clientNotInList(ref ListViewItem itm)
+        {
+            return !parent.clientIsInList(itm);
+        }
+
+        public void addClientToList(ref ListViewItem itm)
+        {
+            parent.addNewClient(itm);
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            ConnectionManager CM = (ConnectionManager)((UdpState)ar.AsyncState).c;
+            UdpClient localListener = (UdpClient)((UdpState)ar.AsyncState).u;
+            IPEndPoint localListenEndPoint = (IPEndPoint)((UdpState)ar.AsyncState).e;
+            Form1 parent = (Form1)((UdpState)ar.AsyncState).p;
+            AutoResetEvent autoEvent = (AutoResetEvent)((UdpState)ar.AsyncState).a;
+
+            try
+            {
+                Byte[] receiveBytes = localListener.EndReceive(ar, ref localListenEndPoint);
+
+                string[] arr = new string[2];
+                arr[0] = localListenEndPoint.Address.ToString();
+                arr[1] = localListenEndPoint.Port.ToString();
+
+                ListViewItem itm = new ListViewItem(arr);
+
+                if (CM.clientNotInList(ref itm))
+                {
+                    CM.addClientToList(ref itm);
+                }
+
+                string receiveString = Encoding.ASCII.GetString(receiveBytes);
+                parent.updateRXTextBox(receiveString);
+                autoEvent.Set();
+            }
+            catch (ObjectDisposedException e)
+            {
+                // The socket was already closed here.
+                return;
+            }
+        }
+
+        public void stopListening()
+        {
+            stop = true;
+            autoEvent.Set();
+        }
+
+        public void sendTextToClients(String text)
+        {
+            // What if were not using UDP??
+            UdpClient listener = stateHandler.u;
+            byte[] send_buffer = Encoding.ASCII.GetBytes(text);
+            
+            ListView.CheckedListViewItemCollection activeClients = parent.getActiveClients();
+
+            IPEndPoint clientEP;
+
+            foreach (ListViewItem client in activeClients)
+            {
+                client.SubItems.
+                clientEP = new IPEndPoint((IPAddress)client.GetSubItemAt(0,0), (int)client.GetSubItemAt(1,0));
+            }
+
+            listener.
+        }
+    }
+}
